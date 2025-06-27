56b220f3895dc88efb22ec13860b1bcf31bec45e
monubawane2861 <monubawane2861>
2025-06-27 20:17:03 +0530

    HT

--- src/components/pages/About/About.jsx
+++ src/components/pages/About/About.jsx
@@ -579,7 +579,7 @@ const About = () => {
             {/* Centering the entire grid using flexbox */}
             <div className="flex justify-center">
               <motion.div
-                className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8"
+                className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-8"
                 variants={containerVariants}
                 initial="hidden"
                 whileInView="visible"
